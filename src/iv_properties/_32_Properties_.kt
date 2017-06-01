967712b994893b555c912e39d59a75de33381a95
Adam Edwards <adamcharlesedwards@gmail.com>
2017-06-01 12:55:42 -0500

    iv_properties

--- src/iv_properties/_32_Properties_.kt
+++ src/iv_properties/_32_Properties_.kt
@@ -3,17 +3,23 @@ package iv_properties
 import util.TODO
 import util.doc32
 
-class PropertyExample() {
+class PropertyExample {
     var counter = 0
-    var propertyWithCounter: Int? = todoTask32()
+    private var _propertyWithCounter: Int? = 0
+    var propertyWithCounter: Int?
+        get() = _propertyWithCounter
+        set(value) {
+            counter++
+            _propertyWithCounter = value
+        }
 }
 
 fun todoTask32(): Nothing = TODO(
-    """
+        """
         Task 32.
         Add a custom setter to PropertyExample.propertyWithCounter so that
         the 'counter' property is incremented every time 'propertyWithCounter' is assigned to.
     """,
-    documentation = doc32(),
-    references = { PropertyExample() }
+        documentation = doc32(),
+        references = { PropertyExample() }
 )
--- src/iv_properties/_33_LazyProperty.kt
+++ src/iv_properties/_33_LazyProperty.kt
@@ -3,11 +3,17 @@ package iv_properties
 import util.TODO
 
 class LazyProperty(val initializer: () -> Int) {
-    val lazy: Int = todoTask33()
+    private val _lazy: Int? = null
+        get() {
+            if (field == null) field = initializer()
+            return field
+        }
+    val lazy: Int
+        get() = _lazy!!
 }
 
 fun todoTask33(): Nothing = TODO(
-    """
+        """
         Task 33.
         Add a custom getter to make the 'lazy' val really lazy.
         It should be initialized by the invocation of 'initializer()'
@@ -15,5 +21,5 @@ fun todoTask33(): Nothing = TODO(
         You can add as many additional properties as you need.
         Do not use delegated properties!
     """,
-    references = { LazyProperty({ 42 }).lazy }
+        references = { LazyProperty({ 42 }).lazy }
 )
